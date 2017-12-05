f545c7cc0a48da1a3c8b774adb389389036c3282
Stephen Sung <stephenrs7@gmail.com>
2017-12-05 02:41:16 -0800

    adpowakd

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,69 @@
 $(".startbutton").on("click", function () {
     $(".startbutton").hide();
-    var container = $("#question-container")
-
+    var container = $("#question-container");
+    var timer = $("#timer-container");
+    startTimer(timer);
     for (var i = 0; i < questions.length; i++) {
         var oneQuestion = questions[i];
         var qValue = oneQuestion.Q;
         var ansValue = oneQuestion.ans;
-        var questiondiv = $("<div>" + qValue + "</div>");
+        var questionDivId = "questionNum" + i;
+        var questiondiv = $('<div id="' + questionDivId + '">' + qValue + "</div>");
         var answerdiv = $("<div>" + ansValue + "</div>");
         container.append(questiondiv);
-
+        var ansForm = $("<form></form>");
+        questiondiv.append(ansForm);
         for (var a = 0; a < ansValue.length; a++) {
-            var ansSelection = $('<div class ="radio"> <label class="radio-inline"><input type="radio" name="optradio">' + ansValue[a] + '</label></div>');
-            container.append(ansSelection);
+            var radioId = questionDivId + "radio" + a;
+            var ansSelection = $('<div class ="radio"> <label class="radio-inline"><input type="radio" id="' + radioId + '" name="optradio">' + ansValue[a] + '</label></div>');
+            ansForm.append(ansSelection);
         }
-        
-        
-
     }
+    setTimeout(endGame, 30000);
+
+
 });
 
+function startTimer(display) {
+    var timer = 30;
+    setInterval(function () {
+        timer--;
+        display.html("Time left: " + timer);
+    }, 1000);
+}
 
+function endGame() {
+
+    var correctAns = 0;
+    var incorrect = 0;
+    var noAns = 0;
+    for (var r = 0; r < questions.length; r++) {
+        var checkedRadioNum = 99;
+        var questionDivId = "questionNum" + r;
+        for (var a = 0; a < 4; a++) {
+            var radioId = questionDivId + "radio" + a;
+            var isChecked = $("#" + radioId).prop("checked");
+            if (isChecked) {
+                checkedRadioNum = a;
+                break;
+            }
+        }
+        if (checkedRadioNum === questions[r].correct) {
+            correctAns++;
+        }
+        else {
+            incorrect++;
+        }
+
+    }
+    var scores = $("#scores");
+    $("#question-container").hide();
+    $("#timer-container").hide();
+    scores.append("Correct: " + correctAns);
+    scores.append("<br></br>");
+    scores.append("Incorrect: " + incorrect);
+
+}
 
 
 
@@ -37,23 +80,28 @@ $(".startbutton").on("click", function () {
 
 var questions = [{
     Q: "What is the capital of New Zealand?",
-    ans: ["Auckland", "Wellington", "Syndey", "Canberra"]
+    ans: ["Auckland", "Wellington", "Syndey", "Canberra"],
+    correct: 1
 },
 {
     Q: "What is the capital of Norway?",
-    ans: ["Helsinki", "Oslo", "Stockholm", "Copenhagen"]
+    ans: ["Helsinki", "Oslo", "Stockholm", "Copenhagen"],
+    correct: 1
 },
 {
     Q: "What is the capital of China?",
-    ans: ["Shanghai", "Beijing", "Taipei", "Pyongyang"]
+    ans: ["Shanghai", "Pyongyang", "Taipei", "Beijing"],
+    correct: 3
 },
 {
     Q: "What is the capital of Indonesia?",
-    ans: ["Jakarta", "Mumbai", "Singapore", "Manila"]
+    ans: ["Jakarta", "Mumbai", "Singapore", "Manila"],
+    correct: 0
 },
 {
-    Q: "What is the capital of Indonesia?",
-    ans: ["Havana", "Buenos Aires", "Kingston", "Port-au-Prince"]
+    Q: "What is the capital of Jamaica?",
+    ans: ["Havana", "Buenos Aires", "Kingston", "Port-au-Prince"],
+    correct: 2
 }
 ]
 
